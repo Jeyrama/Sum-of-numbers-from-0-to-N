2ba74b8503b7b164c77e33a012c75f6368cc4435
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-10 11:57:44 -0700

    Update Sum-of-numbers-from-0-to-N.js

--- Sum-of-numbers-from-0-to-N.js
+++ Sum-of-numbers-from-0-to-N.js
@@ -9,3 +9,26 @@ Output: 0+1+2+3+4+5+6 = 21
 
 
 // Solution
+
+let SequenceSum = (function() {
+  function SequenceSum() {}
+
+  SequenceSum.showSequence = function(count) {
+    let sum = 0;
+    let str = "";
+    if (count === 0) {
+      return "0=0";
+    } else if (count < 0) {
+      return count + "<0";
+    } else {
+      for (var i = 0; i < count; i++) {
+        sum += i;
+        str += i + "+";
+      };
+    sum += count;
+    str += count + " = " + sum;
+    return str;
+    };
+  };
+  return SequenceSum;
+})();
