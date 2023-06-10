3e1a6c573b38ca402425c7d0be8fdd3e38288cbe
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-10 12:05:24 -0700

    Update Sum-of-numbers-from-0-to-N.js

--- Sum-of-numbers-from-0-to-N.js
+++ Sum-of-numbers-from-0-to-N.js
@@ -34,3 +34,11 @@ let SequenceSum = (function() {
 })();
 
 // or
+
+class SumSequence {
+  static showSequence(n) {
+    return n < 0
+      ? `${n}<0`
+      : `${Array.from({length: n+1}, (v, k) => k).join('+')}${n?' = ':'='}${n*(n+1)/2}`;
+  }
+}
